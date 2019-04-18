b8fd810a9837c13b780f93bae9f128f1e92cba4d
Jesper O. Christensen <jc@brandheroes.com>
2019-04-19 00:03:16 +0200

    stuff works

--- main.go
+++ main.go
@@ -1,36 +1,46 @@
 package main
 
 import (
-  "fmt"
 	"database/sql"
   "os"
   "log"
+  "fmt"
 
   "net/http"
   "github.com/prometheus/client_golang/prometheus"
   "github.com/prometheus/client_golang/prometheus/promhttp"
 
   _ "github.com/go-sql-driver/mysql"
+  _ "github.com/joho/godotenv/autoload"
 )
 
-func GetCount(schemadottablename string, column string, db *sql.DB) float64 {
+func GetCount(query string, db *sql.DB) float64 {
   var cnt float64
-  _ = db.QueryRow(`select count(` + column + `) from ` + schemadottablename).Scan(&cnt)
+
+  err := db.QueryRow(query).Scan(&cnt)
+
+  if err != nil {
+    log.Fatal(err)
+  }
   return cnt 
 }
 
 func main() {    
     log.Println("mysql user count started")
    
-    // Open up our database connection.
-    // I've set up a database on my local machine using phpmyadmin.
-		// The database is called testDb
 		var (
+      subsystem = os.Getenv("SUBSYSTEM")
+      name = os.Getenv("NAME")
+      help = os.Getenv("HELP")
 			username = os.Getenv("DB_USER")
       password = os.Getenv("DB_PASSWORD")
+      query = os.Getenv("QUERY")
+      host = os.Getenv("DB_HOST")
+      port = os.Getenv("DB_PORT")
+      database = os.Getenv("DB_NAME")
 		)
 		
-    db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/dbname", username, password))
+    db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, database))
     
     // if there is an error opening the connection, handle it
     if err != nil {
@@ -43,15 +53,15 @@ func main() {
 
     if err := prometheus.Register(prometheus.NewGaugeFunc(
       prometheus.GaugeOpts{
-          Subsystem: "runtime",
-          Name:      "user_count",
-          Help:      "Number of Users that currently exist.",
+          Subsystem: subsystem,
+          Name:      name,
+          Help:      help,
       },
-      func() float64 { return GetCount("mysql.user", "users", db) },
+      func() float64 { return GetCount(query, db) },
     )); err == nil {
         log.Println("GaugeFunc 'user count' registered.")
     }
     
     http.Handle("/metrics", promhttp.Handler())
-	  log.Fatal(http.ListenAndServe("8080", nil))     
+	  log.Fatal(http.ListenAndServe(":8080", nil))     
 }
